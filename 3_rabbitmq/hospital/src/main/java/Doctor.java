ea1dcdeeed10a7e9e351805cdfba4c3a546f0f01
jasieksz <jasiek.sznajd@gmail.com>
2018-04-18 14:58:43 +0200

    using only 1 exchange - something is wrong :(

--- 3_rabbitmq/hospital/src/main/java/Doctor.java
+++ 3_rabbitmq/hospital/src/main/java/Doctor.java
@@ -8,17 +8,18 @@ import java.util.UUID;
 import java.util.concurrent.TimeoutException;
 
 public class Doctor {
-    private final static String QUEUE_NAME = "qDoctor";
-    private final static String EXCHANGE_NAME = "e1";
-    private final static String EXCHANGE_NAME2 = "e2";
+    private final static String QUEUE_EXAMINATION = "doctor";
+    private final static String QUEUE_ADMIN = "admin";
+    private final static String EXCHANGE_HOSPITAL = "hospital";
     private static boolean runFlag = true;
     private final static String corrId = UUID.randomUUID().toString();
 
     public static void main(String[] args) throws IOException, TimeoutException {
-        System.out.println("DOCTOR");
+        System.out.println("DOCTOR " + corrId);
 
         BasicProperties props = new BasicProperties
                 .Builder()
+                .correlationId(corrId)
                 .replyTo(corrId)
                 .build();
 
@@ -28,30 +29,50 @@ public class Doctor {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        //EXCHANGE
-        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
-        channel.exchangeDeclare(EXCHANGE_NAME2, BuiltinExchangeType.TOPIC);
+        // EXCHANGE
+        channel.exchangeDeclare(EXCHANGE_HOSPITAL, BuiltinExchangeType.TOPIC);
 
-        // QUEUE
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME2, corrId);
+        // RECEIVE EXAMINATION RESULTS
+        channel.queueDeclare(QUEUE_EXAMINATION, false, false, false, null);
+        channel.queueBind(QUEUE_EXAMINATION, EXCHANGE_HOSPITAL, EXCHANGE_HOSPITAL + "." + corrId + ".#");
+
+        // RECEIVE ADMIN MESSAGES
+        channel.queueDeclare(QUEUE_ADMIN, false, false, false, null);
+        channel.queueBind(QUEUE_ADMIN, EXCHANGE_HOSPITAL, EXCHANGE_HOSPITAL + ".admin.#");
 
         // CONSUMER
         Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
-                System.out.println("Received : " + message);
+                System.out.println("Received from TECH: " + message);
             }
         };
 
+        Consumer consumerAdmin = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Received from ADMIN: " + message);
+            }
+        };
+
+
+
+        // PROGRAM
+        channel.basicConsume(QUEUE_EXAMINATION, true, consumer);
+        channel.basicConsume(QUEUE_ADMIN, true, consumerAdmin);
+
         while (runFlag) {
-            // receive
-            channel.basicConsume(QUEUE_NAME, true, consumer);
             // read new
-            System.out.println("Enter \"injury type\".\"patient name\" or \"exit\"");
+            System.out.println("Enter injury type or exit");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String message = br.readLine();
+            String injury = br.readLine();
+
+            System.out.println("Enter patient name or exit");
+            String patient = br.readLine();
+
+            String message = "[" + injury + " " + patient + "]";
 
             // exit program
             if (message.contains("exit")) {
@@ -60,8 +81,8 @@ public class Doctor {
             }
             // send
             if (message.contains("knee") || message.contains("elbow") || message.contains("hip")) {
-                channel.basicPublish(EXCHANGE_NAME, message, props, message.getBytes("UTF-8"));
-                System.out.println("Sent : " + message);
+                channel.basicPublish(EXCHANGE_HOSPITAL, EXCHANGE_HOSPITAL + "." + injury, props, message.getBytes("UTF-8"));
+                System.out.println("Sent: " + message);
             } else {
                 System.out.println("Invalid injury type");
             }
--- 3_rabbitmq/hospital/src/main/java/Technician.java
+++ 3_rabbitmq/hospital/src/main/java/Technician.java
@@ -8,15 +8,11 @@ import java.util.UUID;
 import java.util.concurrent.TimeoutException;
 
 public class Technician {
-    private final static String EXCHANGE_NAME = "e1";
-    private final static String EXCHANGE_NAME2 = "e2";
-    private static boolean runFlag = true;
-    private final static String corrId = UUID.randomUUID().toString();
+    private final static String EXCHANGE_HOSPITAL = "hospital";
+    private final static String QUEUE_ADMIN = "admin";
 
     public static void main(String[] args) throws IOException, TimeoutException {
-        System.out.println("TECHNICIAN\nSpecializations :");
-        Arrays.stream(args).forEach(System.out::println);
-
+        System.out.print("TECHNICIAN - Specializations: [" + args[0] + " | " + args[1] + "]\n");
 
         // CHANNEL
         ConnectionFactory factory = new ConnectionFactory();
@@ -25,18 +21,21 @@ public class Technician {
         Channel channel = connection.createChannel();
 
         //EXCHANGE
-        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
-        channel.exchangeDeclare(EXCHANGE_NAME2, BuiltinExchangeType.TOPIC);
+        channel.exchangeDeclare(EXCHANGE_HOSPITAL, BuiltinExchangeType.TOPIC);
 
-        // QUEUE
-        String QUEUE_NAME_1 = args[0];
-        String QUEUE_NAME_2 = args[1];
+        // RECIVE EXAMINATION ORDERS
+        final String QUEUE_SPECIALIZATION_1 = args[0];
+        final String QUEUE_SPECIALIZATION_2 = args[1];
 
-        channel.queueDeclare(QUEUE_NAME_1, false, false, false, null);
-        channel.queueBind(QUEUE_NAME_1, EXCHANGE_NAME, QUEUE_NAME_1 + ".*");
+        channel.queueDeclare(QUEUE_SPECIALIZATION_1, false, false, false, null);
+        channel.queueBind(QUEUE_SPECIALIZATION_1, EXCHANGE_HOSPITAL, EXCHANGE_HOSPITAL + "." + QUEUE_SPECIALIZATION_1 + ".#");
 
-        channel.queueDeclare(QUEUE_NAME_2, false, false, false, null);
-        channel.queueBind(QUEUE_NAME_2, EXCHANGE_NAME, QUEUE_NAME_2 + ".*");
+        channel.queueDeclare(QUEUE_SPECIALIZATION_2, false, false, false, null);
+        channel.queueBind(QUEUE_SPECIALIZATION_2, EXCHANGE_HOSPITAL, EXCHANGE_HOSPITAL + "." + QUEUE_SPECIALIZATION_2 + ".#");
+
+        channel.queueDeclare(QUEUE_ADMIN, false, false, false, null);
+        channel.queueBind(QUEUE_ADMIN, EXCHANGE_HOSPITAL, EXCHANGE_HOSPITAL + ".admin.#");
+
 
         // CONSUMER
         Consumer consumer = new DefaultConsumer(channel) {
@@ -44,24 +43,35 @@ public class Technician {
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
                 System.out.println("Received : " + message);
+                working(5);
+                System.out.println("REPLY TO : " + properties.getReplyTo());
+                channel.basicPublish(EXCHANGE_HOSPITAL, EXCHANGE_HOSPITAL + "." + properties.getReplyTo(), null, (message + "[done]").getBytes("UTF-8"));
+                System.out.println("Sent : " + (message + "[done]"));
+            }
+        };
 
-                message = message + " done";
-                try {
-                    Thread.sleep(4000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                channel.basicPublish(EXCHANGE_NAME2, properties.getReplyTo(), null, message.getBytes("UTF-8"));
-                System.out.println("Sent : " + message);
+        Consumer consumerAdmin = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Received from ADMIN: " + message);
             }
         };
 
         System.out.println("Waiting for messages");
-        channel.basicConsume(QUEUE_NAME_1, true, consumer);
-        channel.basicConsume(QUEUE_NAME_2, true, consumer);
+        channel.basicConsume(QUEUE_SPECIALIZATION_1, true, consumer);
+        channel.basicConsume(QUEUE_SPECIALIZATION_2, true, consumer);
+        channel.basicConsume(QUEUE_ADMIN, true, consumerAdmin);
 
 //        channel.close();
 //        connection.close();
+    }
+
+    private static void working(int time){
+        try {
+            Thread.sleep(time*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
